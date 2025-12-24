f434eb66470d515c2ccbe104be74cff2e4417ab5
yuwenyu <yuwenyu@yuwenyus-MacBook-Pro.local>
2025-12-24 15:16:23 -0500

    Clean up __init__.py files after removing unused modules
    - Remove imports for deleted BERT fine-tuning and gender classification modules
    - Keep only necessary exports for HuBERT trustworthiness training

--- src/tspeech/hubert/data/__init__.py
+++ src/tspeech/hubert/data/__init__.py
@@ -1,7 +1,3 @@
 """HuBERT data module."""
-from tspeech.hubert.data.bert_datamodule import BERTGSTDataModule, BERTGSTDataset
-from tspeech.hubert.data.gender_datamodule import GenderDataModule
-from tspeech.hubert.data.gender_dataset import GenderDataset
-
-__all__ = ["BERTGSTDataModule", "BERTGSTDataset", "GenderDataModule", "GenderDataset"]
+# Data modules for HuBERT training
 
--- src/tspeech/hubert/model/__init__.py
+++ src/tspeech/hubert/model/__init__.py
@@ -1,5 +1,2 @@
 """HuBERT model implementation."""
-from tspeech.hubert.model.bert_gst_encoder import BERTGSTEncoder
-from tspeech.hubert.model.gender_model import GenderModel
-
-__all__ = ["BERTGSTEncoder", "GenderModel"]
+# Models for HuBERT training
